e37eefa863c41e98ea75c240cef476225de4500f
Alex Softer <alwex10@gmail.com>
2014-03-02 12:27:06 +0400

    Finished Lab1

--- lab1/Lab1/Lab1.cpp
+++ lab1/Lab1/Lab1.cpp
@@ -3,10 +3,70 @@
 // максимального і мінімального елементів i -го рядка.
 
 #include "stdafx.h"
+#include<iostream>
+#include<math.h>
+using namespace std;
 
+int max_array(int a[], int num_elements)
+{
+   int i, max=a[0];
+   for (i=1; i<num_elements; i++)
+   {
+	 if (a[i]>max)
+	    max=a[i];
+   }
+   return(max);
+}
+
+int min_array(int a[], int num_elements)
+{
+   int i, min=a[0];
+   for (i=1; i<num_elements; i++)
+   {
+	 if (a[i]<min)
+	    min=a[i];
+   }
+   return(min);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	const int MAX = 20;
+
+	// Enter N
+	int n;
+	cout<<"Enter N"<<endl;
+	cin>>n;
+
+	// Check N
+	if(n>20) {
+		cout<<"N must be not more than 20"<<endl;
+		return 0;
+	}
+
+	// Enter array
+	int a[MAX][MAX];
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			cout<<"A["<<i<<"]["<<j<<"]"<<endl;
+			cin>>a[i][j];
+	    }
+	}
+
+	// Calculations
+	double b[MAX];
+	for(int i = 0; i<n; i++) {
+		int max = max_array(a[i], n);
+		int min = min_array(a[i], n);
+
+		b[i] = (abs(max) + abs(min)) / 2;
+	}
+
+	// Output
+	for(int i = 0; i<n; i++) {
+		cout<<"B["<<i<<"]= "<<b[i]<<endl;
+	}
+
+	cin>>n;
 	return 0;
 }
-
