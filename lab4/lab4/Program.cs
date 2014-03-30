18d4636cde3dcf7e4cca3c1703f0c26fdd06e234
Alex Softer <alwex10@gmail.com>
2014-03-30 12:24:49 +0400

    lab4

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter a, b, c");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+
+            Quadratic q = new Quadratic(a, b, c);
+            Console.WriteLine("Root count = {0}", q.getRootCount());
+            double[] res = q.getRoots();
+            if (res == null)
+            {
+                Console.WriteLine("No roots");
+            }
+            else if (res.Length == 2)
+            {
+                Console.WriteLine("Two roouts = {0}, {1}", res[0], res[1]);
+            }
+            else if(res.Length == 1)
+            {
+                Console.WriteLine("One root = {0}", res[0]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab4/lab4/Quadratic.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab4
+{
+    // Клас «квадратне рівняння»
+    class Quadratic
+    {
+        // Properties
+        protected int a, b, c;
+
+        // Constructor
+        public Quadratic(int _a, int _b, int _c)
+        {
+            a = _a; b = _b; c = _c;
+        }
+
+        // Getters
+        public int getA()
+        {
+            return a;
+        }
+        public int getB()
+        {
+            return b;
+        }
+        public int getC()
+        {
+            return c;
+        }
+
+        // Setters
+        public void setA(int _a)
+        {
+            a = _a;
+        }
+        public void setB(int _b)
+        {
+            b = _b;
+        }
+        public void setC(int _c)
+        {
+            c = _c;
+        }
+
+        // Get root count
+        public int getRootCount()
+        {
+            double d = Math.Pow(b, 2) - 4 * a * c;
+            if (d > 0)
+                return 2;
+            else if (d == 0)
+                return 1;
+            else
+                return 0;
+        }
+
+        // Get roots
+        public double[] getRoots()
+        {
+            double d = Math.Pow(b, 2) - 4 * a * c;
+            if (d > 0)
+            {
+                double x1, x2;
+                x1 = (-b + Math.Sqrt(d))/2*a;
+                x2 = (-b - Math.Sqrt(d)) / 2 * a;
+                return new double[] { x1, x2 };
+            } 
+            else if (d == 0)
+            {
+                double res = -b / 2 * a;
+                return new double[] {res};
+            }
+            else
+                return null;
+
+            return null;
+        }
+    }
+}
